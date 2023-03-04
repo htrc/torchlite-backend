e0f32c1b96611b50684bf9be98c38167ad91708a
cwulfman <cwulfman@princeton.edu>
2023-03-04 17:11:15 -0500

    Adds FilterFactory

--- src/backend/filters.py
+++ src/backend/filters.py
@@ -24,20 +24,32 @@ class Filter(object):
     def __init__(self, *fns):
         self.id = uuid.uuid1()
         self.type = "Generic"
-
-        self.filters = reduce(lambda f, g: lambda x: g(f(x)), fns, lambda x: x)
+        self.filter_fns = fns
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.id})"
 
-    def add_filter(self, fn):
-        if self.filters is None:
-            self.filters = fn
-        else:
-            self.filters = compose(fn, self.filters)
+    def compose(self, f, g):
+        return lambda x: g(f(x))
 
     def apply(self, token_list):
-        return self.filters(token_list)
+        fn = reduce(self.compose, self.filter_fns, lambda x: x)
+        return fn(token_list)
+
+
+class FilterFactory(object):
+    def __init__(self):
+        self.registry = {}
+        self.register('stopwords', torchlite_stopword_filter)
+        self.register('lemmatize', torchlite_lemmatizer)
+        self.register('stem', torchlite_stemmer)
+
+    def register(self, key, fn):
+        self.registry[key] = fn
+
+    def make_filter(self, fnames):
+        filter_fns = [self.registry[fname] for fname in fnames]
+        return Filter(*filter_fns)
 
 
 ws = WorkSet('63f7ae452500006404fc54c7')
