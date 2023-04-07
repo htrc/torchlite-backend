8aa1c7abf64d7975ed5961fae20c054be93e5ee5
cwulfman <cwulfman@princeton.edu>
2023-04-07 09:31:11 -0400

    Changes to dependency injection

--- src/htrc/torchlite/widgets/projectors/__init__.py
+++ src/htrc/torchlite/widgets/projectors/__init__.py
@@ -1,27 +1,25 @@
 from htrc.ef.datamodels import Workset
 from htrc.ef import WorksetEndPoint
+from htrc.torchlite import widgets
 
 
 class Projector:
-    def __init__(self, workset) -> None:
+    def __init__(self, workset: Workset) -> None:
         self.workset = workset
-        self._projection = None
+        self.projection = None
 
-    def generate_projection(self, **kwargs):
+    def project(self, workset, **kwargs):
         pass
-
-    @property
-    def projection(self):
-        if self._projection is None:
-            self._projection = self.generate_projection()
-        return self._projection
 
 
 class TimeLineProjector(Projector):
-    def __init__(self, workset) -> None:
+    def __init__(self, workset: Workset) -> None:
         super().__init__(workset)
 
-    def generate_projection(self, **kwargs):
+    def project(self, **kwargs):
         endpoint = WorksetEndPoint()
         data = endpoint.get_metadata(self.workset.id, 'htid', 'metadata.pubDate')
-        return [{'htid': d.htid, 'pubDate': d.metadata.pubDate} for d in data]
+        self.projection = [
+            {'htid': d.htid, 'pubDate': d.metadata.pubDate} for d in data
+        ]
+        return self.projection
