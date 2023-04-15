ca036dba1f2cfea55e450b1e8584224462fcccd0
cwulfman <cwulfman@princeton.edu>
2023-04-15 09:45:27 -0400

    Adding classes Workset and Api

---
+++ src/htrc/ef/api.py
@@ -0,0 +1,65 @@
+import urllib.parse
+from typing import List, Union
+from pydantic import NoneStr
+import requests
+import htrc.ef.datamodels as ef
+
+# function cleanId(id) {
+#   const i = id.indexOf('.');
+#   if (i == -1) throw `Invalid clean htid: ${id}`;
+#   const lib = id.substring(0, i);
+#   const libId = id.substring(i+1);
+#   return `${lib}.${libId.replaceAll(':', '+').replaceAll('/', '=').replaceAll('.', ',')}`;
+# }
+
+
+def cleanId(id):
+    lib, libId = id.split('.')
+    cleaned = libId.translate(str.maketrans(":/.", "+=,"))
+    return f"{lib}.{cleaned}"
+
+
+class Api:
+    base_uri: str = "https://tools.htrc.illinois.edu/ef-api"
+    worksets_uri: str = f"{base_uri}/worksets"
+    volumes_uri: str = f"{base_uri}/volumes"
+
+    def get(self, uri):
+        r = requests.get(uri)
+        r.raise_for_status()
+        return r.json()['data']
+
+    def get_workset(self, wsid: str) -> ef.Workset:
+        uri = f"{self.worksets_uri}/{wsid}"
+        return ef.Workset(**self.get(uri))
+
+    def get_workset_volumes(self, wsid: str, *fields) -> List[ef.Volume]:
+        uri = f"{self.worksets_uri}/{wsid}/volumes"
+        if fields:
+            uri = f"{uri}?fields={','.join(fields)}"
+        data = self.get(uri)
+        return [ef.Volume(**item) for item in data]
+
+    def get_volume_data(
+        self,
+        htid: str,
+        pos: Union[bool, None] = None,
+        fields: Union[List[str], None] = None,
+    ):
+        clean_htid = cleanId(htid)
+        uri = f"{self.volumes_uri}/{clean_htid}"
+        queries = {}
+        if pos is not None:
+            queries['pos'] = f"{str(pos).lower()}"
+        if fields:
+            queries['fields'] = ','.join(fields)
+
+        if queries:
+            uri = f"{uri}?{urllib.parse.urlencode(queries)}"
+        return self.get(uri)
+
+    def get_volume_metadata(self, htid: str, fields: List[str]):
+        pass
+
+    def get_pages(self, htid: str, seq: List[int], pos: bool, **fields):
+        pass
---
+++ src/htrc/torchlite/worksets.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from typing import List, Union
+import htrc.ef.datamodels as ef
+from htrc.ef import WorksetEndPoint
+
+
+class Workset:
+    """Torchlite Workset
+
+    This class encapsulates data about a workset.  It includes raw
+    data retrieved from the HTRC ef-api, as well as user
+    customizations and modifications.
+
+    """
+
+    def __init__(self, wsid: str) -> None:
+        self._ws_endpoint: WorksetEndPoint = WorksetEndPoint()
+        self.id: str = wsid
+        self._ef_workset: ef.Workset = self._ws_endpoint.get_workset(wsid)
+        self._volumes: Union[List[Volume], None] = None
+
+    def __repr__(self) -> str:
+        return f"Torchlite_Workset({self.id})"
+
+    @property
+    def volumes(self):
+        if self._volumes is None:
+            self._volumes = [Volume(v) for v in self._ws_endpoint.get_volumes(self.id)]
+        return self._volumes
+
+    @property
+    def htids(self):
+        return self._ef_workset.htids
+
+
+class Volume:
+    def __init__(self, data: ef.Volume) -> None:
+        self.data: ef.Volume = data
+        self.htid = self.data.htid
+
+    def __repr__(self) -> str:
+        return f"Torchlite_Volume({self.htid})"
