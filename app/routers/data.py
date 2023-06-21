829fb537cdd01311cdc1fc2e8030975bb8f0925f
cwulfman <cwulfman@princeton.edu>
2023-06-21 12:19:23 -0400

    removes cruft

--- app/routers/data.py
+++ app/routers/data.py
@@ -2,7 +2,6 @@ from typing import Any
 from fastapi import APIRouter, Depends
 import redis.asyncio as redis
 from app.models import torchlite as torchlite
-from app.models import db as db
 from app.config import get_db
 from app.widgets import MapWidget, TimelineWidget
 from app.routers.worksets import read_workset
