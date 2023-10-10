b719e68c082b6c3e6888dddfe3416bd40b875d73
Boris Capitanu <capitanu@illinois.edu>
2023-10-10 13:47:56 -0500

    WIP

--- htrc/torchlite/routers/worksets.py
+++ htrc/torchlite/routers/worksets.py
@@ -20,7 +20,7 @@ async def list_worksets(workset_manager: WorksetManager, author: str | None = No
 @router.get("/{workset_id}/metadata")
 async def get_workset_metadata(workset_id: str, workset_manager: WorksetManager) -> WorksetInfo:
     volumes = await ef_api.get_workset_metadata(workset_id)
-    workset = workset_manager.get_featured_worksets()[workset_id]
+    workset = (await workset_manager.get_featured_worksets())[workset_id]
     volumes_meta = [torchlite_volume_meta_from_ef(vol) for vol in volumes]
     workset_info = WorksetInfo.model_construct(**workset.model_dump(), volumes=volumes_meta)
     return workset_info
