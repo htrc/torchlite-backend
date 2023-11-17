dde9403e2c0003c93859329ef685130965715906
Boris Capitanu <capitanu@illinois.edu>
2023-11-17 16:00:07 -0600

    Fix for PublicationDateTimeline widget data access

--- htrc/torchlite/widgets/publication_date_timeline.py
+++ htrc/torchlite/widgets/publication_date_timeline.py
@@ -4,6 +4,7 @@ from typing import Any, Literal
 from pydantic import BaseModel
 
 from .base import WidgetBase
+from ..ef import models as ef_models
 
 
 class PubDateEntry(BaseModel):
@@ -16,8 +17,8 @@ class PublicationDateTimelineWidget(WidgetBase):
     min_year: int | None = None
     max_year: int | None = None
 
-    async def get_data(self, volumes: dict) -> Any:
-        pub_date_counter = Counter(v['metadata']['pubDate'] for v in volumes if v['metadata']['pubDate'] is not None)
+    async def get_data(self, volumes: list[ef_models.Volume]) -> list[PubDateEntry]:
+        pub_date_counter = Counter(v.metadata.pub_date for v in volumes if v.metadata.pub_date is not None)
         return sorted([
             PubDateEntry.model_construct(year=int(year), count=count)
             for year, count in pub_date_counter.items()
