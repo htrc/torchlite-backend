c47059505fbb67aa2b2d02c7eb28bd23bea08353
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2024-08-22 09:46:59 -0500

    Add check for workset not found.

--- htrc/torchlite/routers/worksets.py
+++ htrc/torchlite/routers/worksets.py
@@ -39,6 +39,8 @@ async def get_workset_metadata(imported_id: str, workset_manager: WorksetManager
 
     volumes = await ef_api.get_workset_metadata(ef_wsid)
     workset = (await workset_manager.get_public_worksets())[imported_id]
+    if not workset:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Workset not found")
     volumes_meta = [torchlite_volume_meta_from_ef(vol) for vol in volumes]
     workset_info = WorksetInfo.model_construct(**workset.model_dump(), volumes=volumes_meta)
     return workset_info
