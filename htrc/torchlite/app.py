460a1d22308e7a6b2563fd57a65a3c06dc857612
Deren Kudeki <dkudeki@illinois.edu>
2024-10-13 16:00:19 -0500

    Need await

--- htrc/torchlite/app.py
+++ htrc/torchlite/app.py
@@ -29,7 +29,7 @@ async def torchlite_startup():
     # Setup backend caching
     try:
         redis = aioredis.from_url(config.REDIS_URL)
-        log.info(redis.ping())
+        log.info(await redis.ping())
         FastAPICache.init(RedisBackend(redis), enable=config.ENABLE_CACHE, prefix=config.REDIS_PREFIX, expire=config.CACHE_EXPIRE, cache_status_header=config.CACHE_STATUS_HEADER)
         log.info('Cache initialized successfully')
     except Exception as e:
