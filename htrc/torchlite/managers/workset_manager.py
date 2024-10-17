6993fa997abc99d5be84a3f9175c416cd3779dba
Deren Kudeki <dkudeki@illinois.edu>
2024-10-16 20:54:44 -0500

    response detail

--- htrc/torchlite/managers/workset_manager.py
+++ htrc/torchlite/managers/workset_manager.py
@@ -42,6 +42,7 @@ class _WorksetManager:
             headers = {'Accept': 'application/json'}
             response = await http.get(f"{config.REGISTRY_API_URL}/worksets", headers=headers)
             print(response)
+            print(response.content)
             data = json.loads(response.content)
             self.user_worksets = {
                 workset['metadata']['id']: WorksetSummary.model_construct(numVolumes=workset['metadata']['volumeCount'],isPublic=workset['metadata']['public'],**workset['metadata'])
