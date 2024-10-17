8cbb3ec74623b7a08d7b3a2240953c4b63ec168a
Deren Kudeki <dkudeki@illinois.edu>
2024-10-16 23:43:31 -0500

    Remove logging from auth

--- htrc/torchlite/auth/auth.py
+++ htrc/torchlite/auth/auth.py
@@ -31,8 +31,6 @@ Token = Annotated[str | None, Depends(fastapi_oauth2)]
 
 
 async def get_current_user(token: Token) -> UserInfo | None:
-    print("get_current_user")
-    print(token)
     user = None
 
     if token:
