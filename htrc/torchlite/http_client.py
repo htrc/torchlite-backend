671c25e4d5b45e9b3902d520c0c7db08119ee937
Deren Kudeki <dkudeki@illinois.edu>
2024-12-02 23:57:51 -0600

    Try not sending cert

--- htrc/torchlite/http_client.py
+++ htrc/torchlite/http_client.py
@@ -2,5 +2,5 @@ import httpx
 from .config import config
 
 http_timeout = httpx.Timeout(5.0, read=None)
-#cert = (config.REGISTRY_TLS_CERT,config.REGISTRY_TLS_KEY) if config.REGISTRY_TLS_CERT and config.REGISTRY_TLS_KEY else None
-http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout)
+cert = (config.REGISTRY_TLS_CERT,config.REGISTRY_TLS_KEY) if config.REGISTRY_TLS_CERT and config.REGISTRY_TLS_KEY else None
+http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout, cert=None)
