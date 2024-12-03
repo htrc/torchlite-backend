6afa50710ab8d937e88ea49225c20724046eadd2
Deren Kudeki <dkudeki@illinois.edu>
2024-12-03 00:08:38 -0600

    Check if values are being loaded

--- htrc/torchlite/http_client.py
+++ htrc/torchlite/http_client.py
@@ -1,6 +1,9 @@
 import httpx
-#from .config import config
+from .config import config
 
 http_timeout = httpx.Timeout(5.0, read=None)
+print("Starting client")
+print(config.REGISTRY_TLS_CERT)
+print(config.REGISTRY_TLS_KEY)
 #cert = (config.REGISTRY_TLS_CERT,config.REGISTRY_TLS_KEY) if config.REGISTRY_TLS_CERT and config.REGISTRY_TLS_KEY else None
 http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout, cert=None)
