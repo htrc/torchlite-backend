630725b9637fa30040082cb33917b1e3217ced33
Deren Kudeki <dkudeki@illinois.edu>
2024-12-03 14:47:42 -0600

    This may be the wrong place to establish credentials

--- htrc/torchlite/http_client.py
+++ htrc/torchlite/http_client.py
@@ -1,10 +1,4 @@
 import httpx
-from .config import config
 
 http_timeout = httpx.Timeout(5.0, read=None)
-print("Have cert")
-print("True" if config.REGISTRY_TLS_CERT else "False")
-print("Have key")
-print("True" if config.REGISTRY_TLS_KEY else "False")
-#cert = (config.REGISTRY_TLS_CERT,config.REGISTRY_TLS_KEY) if config.REGISTRY_TLS_CERT and config.REGISTRY_TLS_KEY else None
-http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout, cert=None)
+http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout)
