a161fdf98cdcb757be3d5d0ec27f6f0f20c00100
Deren Kudeki <dkudeki@illinois.edu>
2024-12-03 19:01:57 -0600

    Create ssl context

--- htrc/torchlite/http_client.py
+++ htrc/torchlite/http_client.py
@@ -1,4 +1,7 @@
-import httpx
+import httpx, ssl
+from .config import config
 
 http_timeout = httpx.Timeout(5.0, read=None)
-http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout)
+ctx = ssl.create_default_context()
+ctx.load_cert_chain(certfile=config.REGISTRY_TLS_CERT, keyfile=config.REGISTRY_TLS_KEY)
+http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout, verify=ctx)
