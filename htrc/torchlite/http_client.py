298f4cc873d2ba2ff6d9e45596e6fad7e5329c42
Deren Kudeki <dkudeki@illinois.edu>
2024-12-04 22:55:41 -0600

    What is being loaded into the context?

--- htrc/torchlite/http_client.py
+++ htrc/torchlite/http_client.py
@@ -5,4 +5,5 @@ from .config import config
 http_timeout = httpx.Timeout(5.0, read=None)
 ctx = ssl.create_default_context()
 ctx.load_cert_chain(certfile=config.REGISTRY_TLS_CERT_PATH, keyfile=config.REGISTRY_TLS_KEY_PATH)
+print(ctx)
 http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout)
