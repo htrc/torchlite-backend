e59b673852557fbf76c2ce454377697c6ab15245
Deren Kudeki <dkudeki@illinois.edu>
2024-12-05 00:47:01 -0600

    Create separate client for mTLS calls to registry

--- htrc/torchlite/http_client.py
+++ htrc/torchlite/http_client.py
@@ -3,7 +3,8 @@ import ssl
 from .config import config
 
 http_timeout = httpx.Timeout(5.0, read=None)
+http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout)
+
 ctx = ssl.create_default_context()
 ctx.load_cert_chain(certfile=config.REGISTRY_TLS_CERT_PATH, keyfile=config.REGISTRY_TLS_KEY_PATH)
-print(ctx)
-http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout, verify=ctx)
+registry_http = httpx.AsyncClient(follow_redirects=True, timeout=http_timeout, verify=ctx)
