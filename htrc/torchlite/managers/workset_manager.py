77b353754a5c59adaf336859c6087bfea40576fa
Deren Kudeki <dkudeki@illinois.edu>
2024-12-06 20:36:22 -0600

    Remove print statement

--- htrc/torchlite/managers/workset_manager.py
+++ htrc/torchlite/managers/workset_manager.py
@@ -28,7 +28,6 @@ class _WorksetManager:
         if self.public_worksets is None:
             headers = {'Accept': 'application/json'}
             response = await registry_http.get(f"{config.REGISTRY_API_URL}/publicworksets", headers=headers)
-            print(response.content)
             data = json.loads(response.content)
             self.public_worksets = {
                 workset['metadata']['id']: WorksetSummary.model_construct(numVolumes=workset['metadata']['volumeCount'],isPublic=workset['metadata']['public'],**workset['metadata'])
