15cf324655ca04ee0a306f1d17db4f296d6d57bb
Deren Kudeki <dkudeki@illinois.edu>
2024-12-12 00:02:15 -0600

    Featured worksets now loading, no need for logs

--- htrc/torchlite/managers/workset_manager.py
+++ htrc/torchlite/managers/workset_manager.py
@@ -70,9 +70,6 @@ class _WorksetManager:
         else:
             wsid_string = str(wsid)
 
-        for ws in self.public_worksets:
-            print(self.public_worksets[ws])
-        print(wsid_string)
         if wsid_string in self.public_worksets or wsid_string in self.user_worksets:
             return True
         else:
