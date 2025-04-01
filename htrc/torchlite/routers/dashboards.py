da7c8889a4c2776d2b29fe91c5a5f196c2a66cbe
Deren Kudeki <dkudeki@illinois.edu>
2025-04-01 15:52:22 -0500

    Check if we have public worksets

--- htrc/torchlite/routers/dashboards.py
+++ htrc/torchlite/routers/dashboards.py
@@ -56,6 +56,7 @@ async def list_dashboards(workset_manager: WorksetManager,
             mongo_client.db["dashboards"].find({"owner": config.TORCHLITE_UID, "isShared": True}).to_list(1000)
         )
         log.debug('returning featured worksets')
+        log.debug(workset_manager.public_worksets)
         return await workset_manager.align_featured_worksets(shared_torchlite_worksets)
 
     if not user:
