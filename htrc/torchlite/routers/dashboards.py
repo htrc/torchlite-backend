e829187a75a9a4be74e5595ce44532ff024086dd
Deren Kudeki <dkudeki@illinois.edu>
2025-05-07 20:47:25 -0500

    Maybe order of functions matters

--- htrc/torchlite/routers/dashboards.py
+++ htrc/torchlite/routers/dashboards.py
@@ -102,6 +102,24 @@ async def create_dashboard(dashboard_create: DashboardCreate,
     return dashboard
 
 
+@router.get("/private", description="Retrieve a private dashboard", response_model_exclude_defaults=True)
+async def get_private_dashboard(user: UserInfo | None = Depends(get_current_user)) -> DashboardSummary:
+    log.debug("get_private_dashboard")
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
+#    user_id = UUID(user.get("htrc-guid", user.sub)) if user else None
+#
+#    dashboard = await DashboardSummary.from_mongo(
+#        mongo_client.db["dashboards"].find_one({"owner": user_id})
+#    )
+#    if dashboard:
+#        log.debug("PRIVATE DASHBOARD")
+#        log.debug(dashboard)
+#        return dashboard
+#    else:
+#        log.error('Dashboard get error')
+#        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 @router.get("/{dashboard_id}", description="Retrieve a dashboard", response_model_exclude_defaults=True)
 @cache(key_builder=request_key_builder)
 async def get_dashboard(dashboard_id: UUID,
@@ -120,22 +138,6 @@ async def get_dashboard(dashboard_id: UUID,
         else:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
         
-@router.get("/private", description="Retrieve a private dashboard", response_model_exclude_defaults=True)
-async def get_private_dashboard(user: UserInfo | None = Depends(get_current_user)) -> DashboardSummary:
-    log.debug("get_private_dashboard")
-    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
-#    user_id = UUID(user.get("htrc-guid", user.sub)) if user else None
-#
-#    dashboard = await DashboardSummary.from_mongo(
-#        mongo_client.db["dashboards"].find_one({"owner": user_id})
-#    )
-#    if dashboard:
-#        log.debug("PRIVATE DASHBOARD")
-#        log.debug(dashboard)
-#        return dashboard
-#    else:
-#        log.error('Dashboard get error')
-#        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.patch("/{dashboard_id}", description="Update a dashboard", response_model_exclude_defaults=True)
 async def update_dashboard(dashboard_id: UUID,
